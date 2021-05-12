6e3af71ba8be5c5d1129d2cfb15af76e64fd0f04
Alexander Welton <Awelto389@west-mec.local>
2021-05-11 17:13:31 -0700

    Fixed README.md

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import logo from '../images/favicon.png'
 const Header = () => {
     return (
         <div>
-            <img id="headerLogo" src={logo}></img>
+            <img id="headerLogo" src={logo} alt="Hold on..."></img>
             <h1 id="header">Weather Potato</h1>
         </div>
     )
