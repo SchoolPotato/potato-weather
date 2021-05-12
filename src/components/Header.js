9cf0f897051707ccb882552f2f2976a0ed16ae67
Alexander Welton <Awelto389@west-mec.local>
2021-05-11 17:50:21 -0700

    Added basic navbar, decided on style/color scheme

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
-import logo from '../images/favicon.png'
+import logo from '../images/logo.png'
 
 const Header = () => {
     return (
-        <div>
-            <img id="headerLogo" src={logo} alt="Hold on..."></img>
-            <h1 id="header">Weather Potato</h1>
+        <div className="navDiv">
+            <nav>
+                <img id="headerLogo" src={logo} alt="Hold on..."></img>
+                <h1 id="header">Weather Potato</h1>
+                <hr id="headerLine"></hr>
+            </nav>
         </div>
     )
 }
